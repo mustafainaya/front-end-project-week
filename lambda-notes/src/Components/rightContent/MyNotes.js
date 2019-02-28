248036f08372b0a9da7a76c5963000ace1503733
mustafa inaya <moose.inaya@icloud.com>
2019-02-28 00:36:04 -0300

    FETCH, Fetching Data. Mapping Over it.

--- lambda-notes/src/Components/rightContent/MyNotes.js
+++ lambda-notes/src/Components/rightContent/MyNotes.js
@@ -4,7 +4,7 @@ const MyNotes = (props) => {
 	return (
 		<div>
 			{props.notes.map((note) => {
-				return <div key={note._id} title={note.title} />;
+				return <div key={note._id} title={note.title} textBody={note.textBody} _id={note._id} {...props} />;
 			})}
 		</div>
 	);
