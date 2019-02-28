24abe398a564ef1417b9af643f088a95692ab1e5
mustafa inaya <moose.inaya@icloud.com>
2019-02-27 23:24:08 -0300

    IMPLEMENT, Implemented axios,React Router Dom, and Link.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+// import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			notes: []
+		};
+	}
+	componentDidMount() {
+		axios
+			.get(`https://fe-notes.herokuapp.com/note/get/all`)
+			.then((res) => {
+				console.log('fetch', res.data);
+				this.setState({
+					notes: res.data
+				});
+			})
+			.catch((err) => {
+				console.log('error', err);
+			});
+	}
+	render() {
+		return <div className="App">{/* <Route exact path="/" render={props} /> */}</div>;
+	}
 }
 
 export default App;
---
+++ lambda-notes/src/Components/leftContent/CreateNote.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class CreateNoteInput extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+}
