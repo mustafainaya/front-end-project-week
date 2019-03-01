57ff211969fbbd60f0654347fd4f07443da6ea5f
mustafa inaya <moose.inaya@icloud.com>
2019-02-28 21:35:30 -0300

    DEBUGG.

--- lambda-notes/src/Components/rightContent/MyNotes.js
+++ lambda-notes/src/Components/rightContent/MyNotes.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MyNotes = (props) => {
 	return (
 		<div>
 			{props.notes.map((note) => {
-				return <div key={note._id} title={note.title} textBody={note.textBody} _id={note._id} {...props} />;
+				return (
+					<NoteDetails key={note._id} title={note.title} textBody={note.textBody} _id={note._id} {...props} />
+				);
 			})}
 		</div>
 	);
 };
 
+const NoteDetails = (props) => {
+	return (
+		<div className="notes">
+			<h3>{props.title}</h3>
+			<p>{props.textBody}</p>
+		</div>
+	);
+};
+
+NoteDetails.default = {
+	title: '',
+	textBody: ''
+};
 export default MyNotes;
