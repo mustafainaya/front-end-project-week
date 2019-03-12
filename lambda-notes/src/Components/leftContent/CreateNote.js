dced6854b637acaf5e841c98509cb31742520361
mustafa inaya <moose.inaya@icloud.com>
2019-03-11 19:49:18 -0700

    Style.

--- lambda-notes/src/Components/leftContent/CreateNote.js
+++ lambda-notes/src/Components/leftContent/CreateNote.js
@@ -42,14 +42,17 @@ class CreateNote extends Component {
 			<div>
 				<InputContainer>
 					<h2>Create a New Note:</h2>
+
 					<form onSubmit={this.addNote}>
-						<input type="text" name="title" onChange={this.changeHandler} value={this.state.note.title} />
-						<input
-							type="text"
-							name="textBody"
-							onChange={this.changeHandler}
-							value={this.state.note.textBody}
-						/>
+						<Input type="text" name="title" onChange={this.changeHandler} value={this.state.note.title} />
+						<TextArea2>
+							<input
+								type="text"
+								name="textBody"
+								onChange={this.changeHandler}
+								value={this.state.note.textBody}
+							/>
+						</TextArea2>
 						<button type="submit">Save</button>
 					</form>
 				</InputContainer>
@@ -64,4 +67,23 @@ const InputContainer = styled.section`
 	width: 100%;
 	margin-left: 30%;
 	width: 100%;
+`;
+const TextArea2 = styled.textarea`
+	width: 70%;
+	height: 350px;
+	max-width: 100%;
+	border: 1px solid black;
+	border-radius: 5px;
+	padding: 2% 1%;
+	margin: 1% 0;
+	font-size: 16px;
+	position: static;
+`;
+const Input = styled.input`
+	width: 40%;
+	border: 1px solid black;
+	border-radius: 5px;
+	padding: 1.5% 1%;
+	margin: 1% 0;
+	font-size: 16px;
 `;
