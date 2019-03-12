58101977eb16a63752766c10465cd826728e991c
mustafa inaya <moose.inaya@icloud.com>
2019-03-11 18:22:37 -0700

    STYLE, Styling Note List.

--- lambda-notes/src/Components/rightContent/MyNotes.js
+++ lambda-notes/src/Components/rightContent/MyNotes.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import viewNotes from '../leftContent/ViewNotes';
+import styled from 'styled-components';
 
 const MyNotes = (props) => {
 	return (
 		<div>
 			{props.notes.map((note) => {
 				return (
-					<NoteDetails key={note._id} title={note.title} textBody={note.textBody} _id={note._id} {...props}>
-						<Link
-							to={`/viewNote/${note._id}`}
+					<NotesContainer>
+						<NoteDetails
+							key={note._id}
 							title={note.title}
 							textBody={note.textBody}
 							_id={note._id}
 							{...props}
-							component={viewNotes}
 						>
-							{' '}
-						</Link>
-					</NoteDetails>
+							<Link
+								to={`/viewNote/${note._id}`}
+								title={note.title}
+								textBody={note.textBody}
+								_id={note._id}
+								{...props}
+								component={viewNotes}
+							>
+								{' '}
+							</Link>
+						</NoteDetails>
+					</NotesContainer>
 				);
 			})}
 		</div>
@@ -40,3 +49,8 @@ NoteDetails.default = {
 	textBody: ''
 };
 export default MyNotes;
+
+const NotesContainer = styled.section`
+width:100%
+display:flex;
+margin-left:25%;`;
